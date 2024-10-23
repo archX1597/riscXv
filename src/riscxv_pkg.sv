1bad6198c9c608d5642dfec3deec682f12e3f683
archX1597 <1047792557@qq.com>
2024-10-24 01:34:53 +0800

    ignore dvt

---
+++ src/riscxv_pkg.sv
@@ -0,0 +1,6 @@
+// riscxv_pkg.sv
+package riscxv_pkg;
+
+    // Define common types, parameters, and constants here
+
+endpackage
---
+++ testbench/riscxv_top_tb.sv
@@ -0,0 +1,11 @@
+// riscxv_top_tb.sv
+module riscxv_top_tb;
+
+    // Input and output ports can be left empty for now
+
+    initial begin
+        $display("Testbench for riscxv started.");
+        // Add simulation initialization here
+    end
+
+endmodule
